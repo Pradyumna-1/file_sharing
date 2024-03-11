a2be43187f8dcfb27de789102fc7eb2b6b16e446
Pradyumna.io <92319505+Pradyumna-1@users.noreply.github.com>
2024-03-11 22:48:50 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,13 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+  {
+    origin:['https://file-sharing-sigma-bice.vercel.app/'],
+    methods:['POST','GET'],
+    credentials:true
+  }
+));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/', router);
